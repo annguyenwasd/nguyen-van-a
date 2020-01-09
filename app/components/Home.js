d4ed0aa21d0d770df30eaf00cb1491c176e7edfe
An Nguyen <an.nguyenwasd@gmail.com>
2020-01-09 21:29:52 +0700

    import json

--- app/components/Home.js
+++ app/components/Home.js
@@ -64,82 +64,6 @@ const initialValues = {
   }
 };
 
-const init = {
-  "sideA": {
-    "people": [
-      {
-        "fullName": "Nguyễn Văn Chúc Ân",
-        "fullNameCap": "",
-        "yearOfBirth": "1993",
-        "identifier": "272 177 205",
-        "idDate": "2007-11-07T17:00:00.000Z",
-        "idLocation": "Đồng Nai",
-        "address": "",
-        "honorific": "Ông",
-        "houseNumber": "471A/10 ấp 94, xã Xuân Thạnh",
-        "district": "Huyện Thống Nhất",
-        "city": "tỉnh Đồng Nai"
-      }
-    ]
-  },
-  "sideB": {
-    "people": [
-      {
-        "fullName": "Nguyễn Thị B",
-        "fullNameCap": "",
-        "yearOfBirth": "1994",
-        "identifier": "272 177 204",
-        "idDate": "2007-01-08T17:00:00.000Z",
-        "idLocation": "TP. HCM",
-        "address": "",
-        "honorific": "Ông",
-        "houseNumber": "1234 Ấp 94 xã Hưng Lộc",
-        "district": "huyện Thống Nhất",
-        "city": "tỉnh Đồng Nai"
-      }
-    ]
-  },
-  "year": 2020,
-  "changes": {
-    "gcn": {
-      "number": "123456",
-      "publish": "12345",
-      "approveDate": "2020-01-09T10:21:36.830Z",
-      "location": "UBND tỉnh Đồng Nai"
-    },
-    "before": {
-      "square": "7899",
-      "number": "7777",
-      "mapNumber": "8888",
-      "purpose": "CLN"
-    },
-    "after": {
-      "square": "7899",
-      "number": "7777",
-      "mapNumber": "8888",
-      "purpose": "CLN"
-    },
-    "reason": "Bán cho người ta"
-  },
-  "contract": {
-    "land": {
-      "squareText": "Bảy nghìn tám trăm chín mươi chín mét vuông",
-      "address": "9898 Ấp 94 xã Hưng Lộc huyện Thống Nhất, tỉnh Đồng Nai",
-      "purposeText": "Mua xây nhà",
-      "duration": "100 năm",
-      "source": "Hông biết",
-      "limitation": "Có xác chết",
-      "types": [{ "name": "Thổ cư", "square": "100", "id": 1578565676643 }],
-      "authenticateLocation": "UBND Xã Hưng Lộc"
-    },
-    "price": { "number": "100 000 000", "text": "Một trăm triệu chẵn" },
-    "authenticateLocation": "",
-    "output": null,
-    "input": null
-  }
-}
-
-
 const toString = date => moment(date).format('DD/MM/YYYY');
 
 const formatPeople = people =>
@@ -191,11 +115,10 @@ const steps = [
   'Nhập thông tin phụ'
 ];
 
-export default function Home() {
-  const [data, setData] = React.useState(init);
+export default function Home({ jsonFile = initialValues }) {
   const [activeStep, setActiveStep] = React.useState(0);
   const { Form } = useForm({
-    defaultValues: data,
+    defaultValues: jsonFile,
     onSubmit: values => {
       handleSubmit(values);
     }
--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,8 +1,94 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
 import Home from '../components/Home';
+import fs from 'fs';
 
-export default class HomePage extends Component {
-  render() {
-    return <Home />;
-  }
+const selections = {
+  NEW_FILE: 0,
+  IMPORT_FILE: 1
+};
+
+export default function HomePage() {
+  const [selection, setSelection] = useState(null);
+  const [filePath, setFilePath] = useState(null);
+  const [file, setFile] = useState(null);
+  const input = useRef(null);
+
+  useEffect(() => {
+    if (filePath) {
+      fs.readFile(
+        filePath,
+        {
+          encoding: 'utf-8'
+        },
+        (err, data) => {
+          if (err) alert(JSON.stringify(err, null, 2));
+          const dataObj = JSON.parse(data);
+          setFile({ ...dataObj, input: null, output: null });
+        }
+      );
+    }
+  }, [filePath]);
+
+  if (selection === selections.NEW_FILE)
+    return (
+      <>
+        <button type="button" onClick={() => setSelection(null)}>
+          Back
+        </button>
+        <Home />
+      </>
+    );
+
+  if (selection === selections.IMPORT_FILE && file)
+    return (
+      <>
+        <button type="button" onClick={() => setSelection(null)}>
+          Back
+        </button>
+        <Home jsonFile={file} />;
+      </>
+    );
+
+  return (
+    <Wrapper>
+      <h1>Chào mừng đến phần mềm xuất file word</h1>
+      <button
+        type="button"
+        onClick={() => {
+          input.current.click();
+          setSelection(selections.IMPORT_FILE);
+        }}
+      >
+        Chọn file data cũ và chỉnh sửa (file có đuôi là json)
+      </button>
+      <button type="button" onClick={() => setSelection(selections.NEW_FILE)}>
+        Tạo file word mới
+      </button>
+      <input
+        ref={input}
+        onChange={e => setFilePath(e.target.files[0].path)}
+        id="output"
+        type="file"
+        accept="application/json"
+      />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  button {
+    display: block;
+    padding: 10px;
+    border-radius: 4px;
+    background-color: chocolate;
+    color: white;
+    border: none;
+    font-size: 18px;
+    margin: 20px;
+  }
+
+  input {
+    visibility: hidden;
+  }
+`;
